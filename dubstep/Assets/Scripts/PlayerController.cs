717abc7534099470ef85e06b262bfb9c2b20a2fa
saranpol <saranpol@saranpols-Mac-mini.local>
2014-06-27 19:44:43 +0700

    test bomb

--- dubstep/Assets/Scripts/PlayerController.cs
+++ dubstep/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
 
 	public GameObject goTerrain;
 	public float speed;
-
+	public GameObject explosion;
 
 	private Vector3 targetPosition;
 
@@ -15,11 +15,27 @@ public class PlayerController : MonoBehaviour {
 	}
 
 
+	public float radius;
+	public float power;
+	void ApplyBomb(Vector3 explosionPos) {
+		Instantiate (explosion, explosionPos, Quaternion.identity);
+
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			if(hit.CompareTag("Player"))
+				continue;
+			if (hit && hit.rigidbody)
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, 3.0F);
+			
+		}
+	}
+
 	public void setTargetPosition(Vector3 pos) {
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(pos);
 		if (goTerrain.collider.Raycast (ray, out hit, Mathf.Infinity)) {
 			targetPosition = hit.point;
+			ApplyBomb(targetPosition);
 		}
 	}
 
--- dubstep/Assets/Scripts/Unit1.cs
+++ dubstep/Assets/Scripts/Unit1.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class Unit1 : MonoBehaviour {
 
 	public float speed;
-
-//	private Vector3 targetPosition;
+	public GameObject explosion;
+	public bool isEnemy;
+	public Material enemyMaterial;
+	//	private Vector3 targetPosition;
 	
+	
+	public void SetupUnit() {
+		if (isEnemy)
+			renderer.material = enemyMaterial;
+	}
 	
 	public void setTargetPosition(Vector3 pos) {
-//		targetPosition = pos;
+		//		targetPosition = pos;
 	}
 	
 	void Update() {
@@ -17,9 +24,25 @@ public class Unit1 : MonoBehaviour {
 		//	transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 		//}
 	}
-
+	
 	void FixedUpdate() {
-		Vector3 movement = new Vector3 (0.0f, 0.0f, 1.0f);
+		Vector3 movement = new Vector3 (0.0f, 0.0f, isEnemy ? -1.0f : 1.0f);
 		rigidbody.AddForce (movement*speed*Time.deltaTime);
 	}
+	
+	//	void OnCollisionEnter(Collision collision) {
+	//		if (collision.gameObject.CompareTag ("Wall Enemy")) {
+	//			Instantiate (explosion, transform.position, transform.rotation);
+	//			Destroy (gameObject);
+	//		}
+	//		if (collision.gameObject.CompareTag ("Wall Player")) {
+	//			Instantiate (explosion, transform.position, transform.rotation);
+	//			Destroy (gameObject);
+	//		}
+	//		if (isEnemy && collision.gameObject.CompareTag ("Player")) {
+	//			Instantiate (explosion, transform.position, transform.rotation);
+	//			Destroy (gameObject);
+	//		}
+	//	}
+
 }
