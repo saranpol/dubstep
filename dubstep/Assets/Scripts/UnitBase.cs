dc01d731eb2a4e39ad28f3e54053a299f6d05d0a
saranpol <saranpol@saranpols-Mac-mini.local>
2014-07-16 19:39:34 +0700

    base

---
+++ dubstep/Assets/Scripts/UnitBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitBase : MonoBehaviour {
+
+	public int health;
+	public int healthMax;
+	public SpriteRenderer spriteGreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spriteGreen.transform.localScale = new Vector3((float)health/(float)healthMax, 1f, 1f);
+	}
+}
